3e33db6816d2992fa165c94ddf7ab3d2109a66dd
İsmail Emre Sırmalı <lynicis@protonmail.com>
2022-11-10 18:21:13 +0300

    update config test

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -11,22 +11,58 @@ import (
 )
 
 func TestReadConfig(t *testing.T) {
-	var err error
+	t.Run("should read config and return config instance", func(t *testing.T) {
+		var err error
 
-	err = os.Setenv(ServerPort, "8080")
-	require.NoError(t, err)
+		err = os.Setenv(ServerPort, "8080")
+		require.NoError(t, err)
 
-	err = os.Setenv(PactBrokerURL, "https://test-broker.com")
-	require.NoError(t, err)
+		err = os.Setenv(PactBrokerURL, "https://test-broker.com")
+		require.NoError(t, err)
 
-	err = os.Setenv(PactBrokerToken, "test-broker-token")
-	require.NoError(t, err)
+		err = os.Setenv(PactBrokerToken, "test-broker-token")
+		require.NoError(t, err)
 
-	var cfg Config
-	cfg, err = ReadConfig()
+		var cfg Config
+		cfg, err = ReadConfig()
+		defer os.Clearenv()
 
-	assert.NoError(t, err)
-	assert.IsType(t, &config{}, cfg)
+		assert.NoError(t, err)
+		assert.IsType(t, &config{}, cfg)
+	})
+
+	t.Run("when read config server port parameter is not defined should return error", func(t *testing.T) {
+		_, err := ReadConfig()
+
+		assert.ErrorIs(t, err, EnvironmentVariablesNotDefined)
+	})
+
+	t.Run("when read config pact broker url parameter is not defined should return error", func(t *testing.T) {
+		var err error
+
+		err = os.Setenv(ServerPort, "8080")
+		require.NoError(t, err)
+
+		_, err = ReadConfig()
+		defer os.Clearenv()
+
+		assert.ErrorIs(t, err, EnvironmentVariablesNotDefined)
+	})
+
+	t.Run("when read config pact broker token parameter is not defined should return error", func(t *testing.T) {
+		var err error
+
+		err = os.Setenv(ServerPort, "8080")
+		require.NoError(t, err)
+
+		err = os.Setenv(PactBrokerURL, "https://test-broker.com")
+		require.NoError(t, err)
+
+		_, err = ReadConfig()
+		defer os.Clearenv()
+
+		assert.ErrorIs(t, err, EnvironmentVariablesNotDefined)
+	})
 }
 
 func TestConfig_GetPactBrokerToken(t *testing.T) {
